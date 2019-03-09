3699a385f38713647587c98330ed7baf817c8bda
Markus Guder <progamer101101@gmx.de>
2019-03-09 13:49:52 +0100

    fixed landing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,12 +4,34 @@ import { HomeService } from '../services/home.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
   dataIsAvailable: boolean = false;
   news = [];
+  current_acts = [
+    {
+      name: "Liquid & Maniac",
+      place: "NH1",
+      time: "11:15 Uhr"
+    },
+    {
+      name: "Blumentopf",
+      place: "Center Stage",
+      time: "13:15 Uhr"
+    },
+    {
+      name: "Rammstein",
+      place: "Center Stage",
+      time: "16:15 Uhr"
+    },
+    {
+      name: "Prinz Pi",
+      place: "Center Stage",
+      time: "22:15 Uhr"
+    }
+  ]
 
   constructor(public homeService: HomeService) {  }
 
